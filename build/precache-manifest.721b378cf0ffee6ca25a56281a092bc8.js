cac639e9332ec9df765c3dacc98e213b4578cf4a
Rautshre <shreyas.raut04@gmail.com>
2025-05-27 20:40:40 +0530

    Removed next.js to fix deploy issue

---
+++ build/precache-manifest.721b378cf0ffee6ca25a56281a092bc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c079a5bf2aa812eafabb6f7aeca9b4ad",
+    "url": "/money_n_wealth/index.html"
+  },
+  {
+    "revision": "6435a9eedf241f729c4d",
+    "url": "/money_n_wealth/static/css/main.145d49bc.chunk.css"
+  },
+  {
+    "revision": "03cf7f7d625e5b1d8c22",
+    "url": "/money_n_wealth/static/js/2.219f2254.chunk.js"
+  },
+  {
+    "revision": "6435a9eedf241f729c4d",
+    "url": "/money_n_wealth/static/js/main.90189ded.chunk.js"
+  },
+  {
+    "revision": "f3b71e05078c05e7d63e",
+    "url": "/money_n_wealth/static/js/runtime~main.bf1205e2.js"
+  }
+]);
